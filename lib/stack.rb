c15d91fa50c26e9de7d69209de6641d20baba449
Michael Dally <mddally@gmail.com>
2022-04-11 13:42:05 -0400

    commit tree 39d3527d448631fadadb367e46c202abb0dbd617

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,45 @@
 # your code here
+class Stack
+    def initialize(limit=Float::INFINITY)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        if(@limit == 0)
+            raise StandardError.new("Stack Overflow")
+        else
+            @stack.push(value)
+            @limit -= 1
+        end
+    end
+
+    def pop
+        @limit += 1
+        @stack.pop
+    end
+
+    def peek
+        @stack[@stack.length-1]
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.length == 0
+    end
+
+    def full?
+        @limit == 0
+    end
+
+    def search(target)
+        @stack.each_with_index do |item, idx|
+          return size - idx - 1 if item == target
+        end
+        -1
+      end
+
+end
